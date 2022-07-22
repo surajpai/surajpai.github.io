7cd799d9524de2a90d94b4f23ef3e082c29c67d8
Suraj Pai <pai.suraj@pwc.com>
2022-07-22 15:59:51 +0100

    New build

--- build/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { A as ActiveRouter } from './active-router-9a6f7aec.js';
-import './match-path-760e1797.js';
-import './index-e2cdb0b4.js';
-import './location-utils-fea12957.js';
-
-function injectHistory(Component) {
-    ActiveRouter.injectProps(Component, ['history', 'location']);
-}
